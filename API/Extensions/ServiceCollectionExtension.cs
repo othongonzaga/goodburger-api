296b348a60a4c2438b5d6c2100fdc14a9a3680b9
othongonzaga <othoncesar1@hotmail.com>
2026-04-21 00:48:05 -0300

    Adicionar arquivos de projeto.

---
+++ API/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using goodburger_api.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace goodburger_api.API.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection")
+            ?? "Data Source=goodburger.db";
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite(connectionString));
+
+        return services;
+    }
+}
